19301526ba8bddcc307fd65ce45f29aa8aa58626
Phol <Pholibertlim@gmail.com>
2023-01-20 21:32:30 +0800

    updated UI each pokemon card

--- src/Components/PokemonDetails/index.jsx
+++ src/Components/PokemonDetails/index.jsx
@@ -21,8 +21,29 @@ const PokemonDetails = () => {
   };
 
   const displayTypes = (e) => {
+    const color = (
+      e.type.name === 'fire'? 'orange':
+      e.type.name === 'water'? 'blue':
+      e.type.name === 'grass'? 'green':
+      e.type.name === 'poison'? 'purple':
+      e.type.name === 'flying'? 'darkviolet':
+      e.type.name === 'fighting'? 'red':
+      e.type.name === 'bug'? 'lightgreen':
+      e.type.name === 'electric'? 'yellow':
+      e.type.name === 'rock'? 'darkorange':
+      e.type.name === 'ground'? 'brown':
+      e.type.name === 'dark'? 'black':
+      e.type.name === 'fairy'? 'pink':
+      e.type.name === 'normal'? 'darkgray':
+      e.type.name === 'ice'? 'lightblue':
+      e.type.name === 'dragon'? 'violet':
+      e.type.name === 'psychic'? 'darkblue':
+      e.type.name === 'ghost'? '#B689FA':
+      e.type.name === 'steel'? '#D8DDCB':
+      null
+    )
     return (
-      <Typography variant="h6" key={e.type.name} sx={{ width: '100px', margin: '2px 5px', backgroundColor: 'darkgray', color: 'black', padding: '2px 10px', borderRadius: '20px', fontVariant: 'small-caps' }}>
+      <Typography variant="h6" key={e.type.name} sx={{ width: '100px', margin: '2px 5px', backgroundColor:`${color}`, color: 'white', padding: '2px 10px', borderRadius: '5px', fontVariant: 'small-caps',textShadow:'1px 1px 4px #000000' }}>
         {e.type.name}
       </Typography>
     );
@@ -30,7 +51,7 @@ const PokemonDetails = () => {
 
   const displayMoves = (e) => {
     return (
-      <Typography key={e.move.name} sx={{ width: '150px', margin: '2px 8px', backgroundColor: 'darkgray', color: 'black', padding: '2px 10px', borderRadius: '5px' }}>
+      <Typography key={e.move.name} sx={{ width: '150px', margin: '2px 8px', backgroundColor: 'darkgray', color: 'white',textShadow:'1px 1px 4px #000000', padding: '2px 10px', borderRadius: '5px' }}>
         {e.move.name}
       </Typography>
     );
@@ -44,12 +65,12 @@ const PokemonDetails = () => {
   return (
     <Stack alignItems="center" width="50vw" sx={{ backgroundColor:{md:'gray', sm:'none'}, margin: '20px auto', borderRadius: '20px', padding: '50px' }}>
       <Link to={`/${typeId}`}>
-        <Typography width="fit-content" sx={{ color: 'white', marginLeft: '-20vw', padding: '2px 25px' }}>
+        <Typography width="fit-content" sx={{ color: 'white', marginLeft: '-25vw', padding: '2px 5px', textShadow:' 1px 1px 4px #000000' }}>
           Back
         </Typography>
       </Link>
       <img src={state.artwork} alt="artWork" width="350px" />
-      <IconButton disabled={state.disable} onClick={captured} size="large" edge="start" aria-label="menu" sx={{ borderRadius: '15px', color: 'white', backgroundColor: '#0A0908' }}>
+      <IconButton disabled={state.disable} onClick={captured} size="large" edge="start" aria-label="menu" sx={{ borderRadius: '5px', color: 'white', backgroundColor: 'darkgray', border:'1px solid black', textShadow:'1px 1px 4px #000000' }}>
         <CatchingPokemonIcon fontSize="inherit" />
         <Typography>Capture</Typography>
       </IconButton>
